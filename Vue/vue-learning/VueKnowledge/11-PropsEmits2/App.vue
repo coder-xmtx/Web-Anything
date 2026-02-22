09ca7c7afe3d1f682547141bede47a1c79ed9c56
coder-xmtx <mail_xmtx@163.com>
2026-02-22 17:17:05 +0800

    feat : Vue Learning
    - props and emits
    - composables

---
+++ Vue/vue-learning/VueKnowledge/11-PropsEmits2/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Child from './Child.vue';
+
+const handleMessage = (message) => {
+    console.log(message);
+}
+
+</script>
+
+<template>
+    <Child @response="handleMessage" />
+</template>
+
+<style scoped></style>
---
+++ Vue/vue-learning/VueKnowledge/11-PropsEmits2/Child.vue
@@ -0,0 +1,15 @@
+<script setup>
+const emit = defineEmits(["response"]);
+
+const sendMessage = () => {
+    emit("response", "hello");
+}
+
+</script>
+
+<template>
+    <button @click="sendMessage"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer duration-150">发送</button>
+</template>
+
+<style scoped></style>
