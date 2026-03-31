1d7a473c965c0aefe470d12e23d7653f7ced4178
coder-xmtx <mail_xmtx@163.com>
2026-04-01 00:04:27 +0800

    feat : try to create a todo project with vue - Vue3 TailwindCSS DaisyUI

--- TestProject/Todo-List/src/App.vue
+++ TestProject/Todo-List/src/App.vue
@@ -1,11 +1,17 @@
-<script setup></script>
+<script setup>
+import { RouterView } from 'vue-router';
+import NavBar from './components/NavBar.vue';
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <nav class="px-4 sm:px-6 lg:px-8">
+    <NavBar />
+  </nav>
+
+  <main class="px-4 sm:px-6 lg:px-8">
+    <RouterView />
+  </main>
 </template>
 
 <style scoped></style>
---
+++ TestProject/Todo-List/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+
+</script>
+
+<template>
+    <h1 class="text-center text-3xl font-bold">Todo List</h1>
+</template>
+
+<style scoped></style>
